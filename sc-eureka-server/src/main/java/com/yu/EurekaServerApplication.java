eb745f3153d83d05278462618d75b2fd4349b397
object-not-found <439110898@qq.com>
2017-09-02 15:45:51 +0800

    eureka  server

---
+++ sc-eureka-server/src/main/java/com/yu/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.yu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
